10372498e852b68488ff79ccad1bbf65efbe333f
jensnyder <jlsnyder09@gmail.com>
2018-12-08 10:43:46 -0500

    Set up start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 //set up to allow us to use partials for templating
@@ -76,6 +77,6 @@ app.get("/bad", (req, res) => {
 
 //binds application to localhost
 //second optional argument - function
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
